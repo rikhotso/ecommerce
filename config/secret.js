8e49f4e0fff5db14c36617e31bb68c6d1d30f412
dashrikhotso <ntwananor@gmail.com>
2016-12-03 09:43:09 +0200

    Revert "first commit"
    This reverts commit 036acd2bb15577fccfe85e22b344f128e929a332.

--- config/secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	database: 'mongodb://localhost:27017/demodb',
-	port: 3000,
-	secretKey: "P@$w0rd101",
-}
--- routes/about.js
+++
@@ -1,7 +0,0 @@
-var router = require('express').Router();
-
-router.get('/about', function(req, res){
-	res.render('main/about');
-});
-
-module.exports = router;
